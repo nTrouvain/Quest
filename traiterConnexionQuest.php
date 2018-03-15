112c9e21d09b57204b6eb14e17d63ac78a27d45c
mdevaux001 <mdevaux001@ensc.fr>
2018-03-15 12:09:16 +0100

    connexion à jour

--- traiterConnexionQuest.php
+++ traiterConnexionQuest.php
@@ -1,7 +1,8 @@
 <?php session_start();
 require("connect_to_quest.php");
 
-$validation=false;
+$validation_exp=false;
+$validation_user=false;
 if (!empty($_POST['mail']) and !empty($_POST['mdp'])) {
     
     $mail = $_POST['mail'];
@@ -16,18 +17,36 @@ if (!empty($_POST['mail']) and !empty($_POST['mdp'])) {
          }
        
         // Authentication successful
-                $_validation=true;
+                $_validation_exp=true;
                 $_SESSION['idEXP']=$idEXP;
         
 
     }
-    else {
-        $error = "Utilisateur non reconnu";
-        print "utilisateur non reconnu";
+    
+       
+    $requete =$BDD->prepare('select usr_id from user where usr_mail=? and usr_mdp=?');
+    $requete->execute(array($mail, $mdp));
+     if ($requete->rowCount() == 1 ) 
+    {
+        while ($row = $requete->fetch())
+         {
+            $idUSER=$row['usr_id'];
+         }
+       
+        // Authentication successful
+                $_validation_user=true;
+                $_SESSION['idUSER']=$idUSER;
+        
+
     }
-if ($_validation)
+    
+if ($_validation_exp)
     {
         header("Location: http://localhost/web/Quest/PageAcceuilEXP.php");
+    }
+else if ($_validation_user)
+    {
+        header("Location: http://localhost/web/Quest/PageAccueilUSER.php");
     }
 else {header("Location: http://localhost/web/Quest/connexionQuest.html");}
     
